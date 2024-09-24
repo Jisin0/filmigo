a868d4fd37d35a166765897b3b3aec2109af17a9
Jisin0 <josejisin@gmail.com>
2024-09-24 13:18:18 +0100

    feat(omdb): prettyprint much more information
    Old output with emojis and symbols is replaced with a generic output with full information

--- omdb/prettyprint.go
+++ omdb/prettyprint.go
@@ -3,33 +3,89 @@
 
 package omdb
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	// default string returned if empty
+	notAvailable = "N/A"
+)
 
 // PrettyPrint prints out movie data in a neat interface.
 func (m *Movie) PrettyPrint() {
-	fmt.Printf("%s(%s)", m.Title, m.Year)
+	fmt.Printf("Id: %s\n", m.ImdbID)
+	fmt.Printf("Type: %s\n", m.Type)
+	fmt.Printf("Title: %s", m.Title)
+	fmt.Printf("\nYear: %s", m.Year)
 
-	if m.Rated != "" {
-		fmt.Printf(" [%s Rated]", m.Rated)
+	if m.ImdbRating != notAvailable {
+		fmt.Printf("\nRating: %s\nVotes: %s", m.ImdbRating, m.ImdbVotes)
+	}
+
+	if m.Released != notAvailable {
+		fmt.Printf("\nReleased: %s", m.Released)
+	}
+
+	if m.Country != notAvailable {
+		fmt.Printf("\nCountry: %s", m.Country)
+	}
+
+	if m.Rated != notAvailable {
+		fmt.Printf("\nContent Rated: %s", m.Rated)
+	}
+
+	if m.Runtime != notAvailable {
+		fmt.Printf("\nRuntime: %s", m.Runtime)
+	}
+
+	if m.Genres != notAvailable {
+		fmt.Printf("\nGenres: %s", m.Genres)
+	}
+
+	if m.Languages != notAvailable {
+		fmt.Printf("\nLanguages: %s", m.Languages)
+	}
+
+	if m.DVD != notAvailable {
+		fmt.Printf("\nDVD Release: %s", m.DVD)
+	}
+
+	if m.Actors != notAvailable {
+		fmt.Printf("\nActors: %s", m.Actors)
+	}
+
+	if m.Director != notAvailable {
+		fmt.Printf("\nDirectors: %s", m.Director)
+	}
+
+	if m.Writers != notAvailable {
+		fmt.Printf("\nWriters: %s", m.Writers)
+	}
+
+	if m.Production != notAvailable {
+		fmt.Printf("\nProducers: %s", m.Production)
+	}
+
+	if m.Poster != notAvailable {
+		fmt.Printf("\nPoster: %s", m.Poster)
+	}
+
+	if m.Awards != notAvailable {
+		fmt.Printf("\nAwards: %s", m.Awards)
+	}
+
+	if m.BoxOffice != notAvailable {
+		fmt.Printf("\nIncome: %s", m.BoxOffice)
+	}
+
+	if m.Website != notAvailable {
+		fmt.Printf("\nWebsite: %s", m.Website)
+	}
+
+	if m.Plot != notAvailable {
+		fmt.Printf("\nPlot: %s", m.Plot)
 	}
 
 	fmt.Println()
-
-	if m.ImdbRating != "" {
-		fmt.Printf("⭐%s | %s❤️", m.ImdbRating, m.ImdbVotes)
-	}
-
-	fmt.Print("\n\n", m.Plot, "\n\n")
-
-	if m.Released != "" {
-		fmt.Printf("Released: %s\n", m.Released)
-	}
-
-	if m.Runtime != "" {
-		fmt.Printf("Runtime: %s\n", m.Runtime)
-	}
-
-	if m.Genres != "" {
-		fmt.Printf("Genres: %s\n", m.Genres)
-	}
 }
